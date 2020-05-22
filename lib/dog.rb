47f8ad5b562b423948cccb02e807bc51eaff1595
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-22 21:43:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,8 @@
 class Dog
+  lassie = Dog.new
+lassie.name = "Lassie"
+ 
+lassie.name #=> "Lassie"
   def name=(dog_name)
     this_dogs_name = dog_name
   end
@@ -6,10 +10,5 @@ class Dog
   def name
     this_dogs_name
   end
-  
-  lassie = Dog.new
-lassie.name = "Lassie"
- 
-lassie.name #=> "Lassie"
 end
 
